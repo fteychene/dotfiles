d5ab75429bcf60f10d36481862b97bdfed602e93
fteychene <francois.teychene@gmail.com>
2021-09-17 16:15:24 +0200

    Update rofi configuration
    Update screenlayout after update

--- screenlayouts/.screenlayout/laptop.sh
+++ screenlayouts/.screenlayout/laptop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
+xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output VIRTUAL1 --off
 feh --bg-scale ~/Pictures/wallpapers/green-fractal-wallpaper-1920x1200.jpg
